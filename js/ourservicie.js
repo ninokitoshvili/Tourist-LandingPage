802db15ab6a99bef4e921cc584ca66589ec1d08a
ninokitoshvili <nino.kitoshvili.3@iliauni.edu.ge>
2023-04-28 22:33:21 +0400

    finished our servicies

--- js/ourservicie.js
+++ js/ourservicie.js
@@ -1,79 +1,72 @@
 const images = [
-    './static/images/ourServicies/1.png','./static/images/ourServicies/2.png',    './static/images/ourServicies/3.png','./static/images/ourServicies/4.png',    './static/images/ourServicies/5.png','./static/images/ourServicies/6.png'
-            ];
-            const titles = [
-                'flight booking','hotel & resort booking','family trip planner', 'cruise tour', 'fire camp', 'corporate holidays'
-                        ];            
-                        const informations = [
-                            'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.'
-                                    ];
-         
-            const container = document.getElementById('ourservice-container');
-         
-            for (let i = 0; i < images.length; i++) {
-                const div=document.createElement('div');
-                const img = document.createElement('img');
-                const h4=document.createElement('h4');
-                const p=document.createElement('p');
-                img.src = images[i];
-                img.alt=titles[i];
-                div.id='img'+i;
-                
-                const title = document.createTextNode(titles[i]);
-                const information = document.createTextNode(informations[i]);
-                div.className = "flex-btw";
-                h4.appendChild(title);
-                p.appendChild(information);
-                div.appendChild(img);
-                div.appendChild(h4);
-                div.appendChild(p);
-                container.appendChild(div);
-                div.onclick=() => (console.log(div.id));
-            //    console.log(div.id);
-               
-               
-            }
+	"./static/images/ourServicies/1.png",
+	"./static/images/ourServicies/2.png",
+	"./static/images/ourServicies/3.png",
+	"./static/images/ourServicies/4.png",
+	"./static/images/ourServicies/5.png",
+	"./static/images/ourServicies/6.png",
+];
+const titles = [
+	"flight booking",
+	"hotel & resort booking",
+	"family trip planner",
+	"cruise tour",
+	"fire camp",
+	"corporate holidays",
+];
+const informations = [
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.",
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.",
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.",
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.",
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.",
+	"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem.",
+];
 
-            // document.getElementById(div.id).addEventListener("onclick", hover(div.id));
+const container = document.getElementById("ourservice-container");
 
-            function hover(){
-                console.log('s')
-                // for (let i = 0; i < images.length; i++) {
-                //     const div=document.getElementById('img'+i);
-                //     console.log(div)
+for (let i = 0; i < images.length; i++) {
+	const div = document.createElement("div");
+	const img = document.createElement("img");
+	const h4 = document.createElement("h4");
+	const p = document.createElement("p");
 
-                // }
+	const title = document.createTextNode(titles[i]);
+	const information = document.createTextNode(informations[i]);
+
+	img.src = images[i];
+	img.alt = titles[i];
+	img.className = "inner zoomD";
+
+	div.id = "img" + i;
+	div.className = "flex-btw  imgmedia block bg-hover-aqua";
+
+	h4.appendChild(title);
+	p.appendChild(information);
+	div.appendChild(img);
+	div.appendChild(h4);
+	div.appendChild(p);
+	container.appendChild(div);
+	div.onclick = () => {
+		console.log(i);
+		let lightbox = document.getElementById("lightbox");
+		let lightboxContainer = document.createElement("container");
+		let imgShow = document.createElement("img");
+		let h4Show = document.createElement("h4");
+		let pShow = document.createElement("p");
+		const titleShow = document.createTextNode(titles[i]);
+		const informationShow = document.createTextNode(informations[i]);
+		h4Show.appendChild(titleShow);
+		pShow.appendChild(informationShow);
+		imgShow.src = images[i];
+		imgShow.alt = titles[i];
+		lightboxContainer.className = "lightboxContainer";
+		lightbox.innerHTML = "";
+		lightboxContainer.appendChild(imgShow);
+		lightboxContainer.appendChild(h4Show);
+		lightboxContainer.appendChild(pShow);
+		lightbox.appendChild(lightboxContainer);
+		lightbox.className = "show cursor";
+	};
+	lightbox.onclick = () => (lightbox.className = "");
 }
-
-// window.onload=()=>{
-//     for (let i = 0; i < images.length; i++) {
-//        let  div=document.getElementById("img"+i)
-//         div.onclick = () => (console.log("img"+i));
-//     }
-// }
-
-
-
-// window.onload = () => {
-// 	let all = document.getElementsByClassName("zoomD"),
-// 		lightbox = document.getElementById("lightbox"),
-// 		outer = document.getElementsByClassName("outer");
-
-// 	if (outer.length > 0) {
-// 		for (let i of outer) {
-// 			i.onclick = () => {
-// 				for (var j = 0; j < all.length; j++) {
-// 					if (i == outer[j]) {
-// 						let clone = all[j].cloneNode(true);
-// 						clone.className = "";
-// 						lightbox.innerHTML = "";
-// 						lightbox.appendChild(clone);
-// 						lightbox.className = "show";
-// 					}
-// 				}
-// 			};
-// 		}
-// 	}
-
-// 	lightbox.onclick = () => (lightbox.className = "");
-// };
